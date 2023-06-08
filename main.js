ab0c71438734bc27649b8d37d0f356e43286547f
Astrid Fh <astfje94@hotmail.com>
2023-06-08 11:06:41 +0200

    Updates

---
+++ main.js
@@ -0,0 +1,56 @@
+
+
+function scrollFunction() {
+  const scroll = document.getElementById("aboutPage");
+  scroll.scrollIntoView();
+}
+
+
+function scrollFunction2() {
+  const scroll = document.getElementById("skillsPage");
+  scroll.scrollIntoView();
+}
+
+function scrollFunction3() {
+  const scroll = document.getElementById("projectsPage");
+  scroll.scrollIntoView();
+}
+
+
+window.onbeforeunload = function(){
+  window.scrollTo(0, 0);
+}
+
+
+/*
+function aboutMe() {
+  conmst
+  button.addEventListener(`click`, () = > {
+  document.getElementById("aboutMe").innerText = "About me"
+  
+  })
+}
+
+function skills() {
+  document.getElementById("skills").innerText = "Skills"
+}
+
+function projects() {
+  document.getElementById("projects").innerText = "Projects"
+}
+
+
+
+function emailBtn(){
+
+}
+
+
+
+function aboutMe2 () {
+const button = document.createElement("button");
+button.innerText = "About me"
+button.addEventListener(`click`, () => {
+  document.getElementById("menu").innerText = "Hellåå"
+})
+*/
